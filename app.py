44f250280e578a0925224289c68686bec0eef59f
spennington <spennington13@gmail.com>
2021-01-30 14:33:57 -0500

    app.py added
    what was not changed?....

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_pymongo import PyMongo
+from bson.json_util import dumps
+
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/indego_db"
-mongo = PyMongo(app)
+connect = PyMongo(app, uri = "mongodb://localhost:27017/indego_db")
 
 @app.route("/")
-def home_page():
-    online_users = mongo.db.users.find({"online": True})
-    return render_template("index.html",
-        online_users=online_users)
+def home():
+    indego = connect.db.summary.find()
+    data = []
+
+    for doc in list(indego):
+        row = {}
+        row['index'] = doc['index']
+        row['Q2-19'] = doc['Q2-19']
+        row['Q2-20'] = doc['Q2-20']
+        row['Q3-20'] = doc['Q3-20']
+        row['Q3-19'] = doc['Q3-19']
+        data.append(row)
+    # return dumps(list(indego))
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)

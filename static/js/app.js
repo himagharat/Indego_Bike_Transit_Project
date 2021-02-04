689d3178d446f3a3078d65f3fcc98ba85c6ebb22
spennington <spennington13@gmail.com>
2021-02-03 21:34:55 -0500

    app.js

--- static/js/app.js
+++ static/js/app.js
@@ -9,38 +9,80 @@ for (var key in x){
   y.push(key);
   console.log(y);
 }
+var horizontalBarChartData ={
+  labels:[],
+  datasets:[{
+    data: data[0].Q219
+    // data[1].Q219,
+    // data[2].Q219,
+  }] 
+};
+
 window.onload = function() {
-var chart = new CanvasJS.Chart("bar",{
-  animationEnabled: true,
+  var ctx = document.getElementById('bar').getContext('2d');
+  window.myHorizontalBar = new Chart(ctx, {
+    type: 'bar',
+    data: horizontalBarChartData,
+    options: {
+      indexAxis: 'y',
+      // Elements options apply to all of the options unless overridden in a dataset
+      // In this case, we are setting the border of each horizontal bar to be 2px wide
+      elements: {
+        bar: {
+          borderWidth: 2,
+        }
+      },
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'right',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Horizontal Bar Chart'
+        }
+      }
+    }
+  });
 
-  title:{
-    text: "Passholder Type"
-  },
+};
 
-  axisX:{
-    interval: 1
-  },
+// window.onload = function() {
+// var chart = new CanvasJS.Chart("bar",{
+//   animationEnabled: true,
 
-  axisY2:{
-    interlacedColor: "rgba(1,77,101,.2)",
-      gridColor: "rgba(1,77,101,.1)",
-      title: "Number of Passes"
-  },
+//   title:{
+//     text: "Passholder Type"
+//   },
 
-  data:[{
-    type: "bar",
-    name: "companies",
-    axisYType:"secondary",
-    color: "#014D65",
-    dataPoints:[
-      {y:data[0].Q219, label: 'Q219'},
-      {y:data[0].Q319, label:'Q319'},
-      {y:data[0].Q220, label:'Q220'},
-      {y:data[0].Q320, label: 'Q320'}
-    ]
-  }]
-});
+//   axisX:{
+//     interval: 1
+//   },
 
-chart.render();
+//   axisY2:{
+//     interlacedColor: "rgba(1,77,101,.2)",
+//       gridColor: "rgba(1,77,101,.1)",
+//       title: "Number of Passes"
+//   },
 
-}
+//   data:[{
+//     type: "bar",
+//     name: "companies",
+//     axisYType:"secondary",
+//     color: "#014D65",
+//     dataPoints:[
+//       {y:data[0].Q219, label: 'Quarter 2 2019 Indego 30'},
+//       {y:data[1].Q219, label: 'Quarter 2 2019 Indego 365'},
+//       {y:data[2].Q219, label: 'Quarter 2 2019 Daily Pass'},
+//       {y:data[3].Q219, label: 'Quarter 2 2019 IndegoFlex'},
+//       {y:data[4].Q219, label: 'Quarter 2 2019 Walk-Up'},
+//       {y:data[0].Q319, label:'Q319'},
+//       {y:data[0].Q220, label:'Q220'},
+//       {y:data[0].Q320, label: 'Q320'}
+//     ]
+//   }]
+// });
+
+// chart.render();
+
+// }

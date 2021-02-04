56cff54fe2ca989e29d4d379be7c35168769ab87
spennington <spennington13@gmail.com>
2021-02-03 20:42:27 -0500

    app.js
    FINALLY HAVE A WORKING GRAPH!

--- static/js/app.js
+++ static/js/app.js
@@ -9,44 +9,38 @@ for (var key in x){
   y.push(key);
   console.log(y);
 }
+window.onload = function() {
+var chart = new CanvasJS.Chart("bar",{
+  animationEnabled: true,
 
-// function getPlots(index) {
-//   d3.json(data).then((sampleData) => {
+  title:{
+    text: "Passholder Type"
+  },
 
-//   // collect data for bar and bubble plots
-//   var sdata = sampleData.index.filter(d1 => d1.id.toString() === id)[0];
-//     console.log(sdata);
-//   // // collect sample values for bar chart with top 10 OTU values
-//   // var sample_values = sdata.sample_values.slice(0,10).reverse();
-//   var index = (sdata.index);
-  // console.log(`OTU_top: ${OTU_top}`);
-  // var OTU_id = OTU_top.map(data => "OTU " + data);
-  // console.log(`OTU_id: ${OTU_id}`);
-  // var labels = sdata.otu_labels.slice(0,10);
+  axisX:{
+    interval: 1
+  },
 
-  // /// BAR CHART AREA
-  // var trace1 = {
-  //     x: sample_values,
-  //     y: OTU_id,
-  //     text: labels,
-  //     marker: {
-  //         color: 'rgb (142, 124, 195)'},
-  //     type: "bar",
-  //     orientation: "h"
-  // };
-  // // create data variable for bar plot
-  // var chartBar = [trace1];
-  // // apply the group bar mode to the layout
-  // var layout1 = {
-  //     title: "Values of Top 10 Samples",
-  //     yaxis: {
-  //         tickmode: "linear",
-  //     },
-  //     margin: {
-  //         l: 100,
-  //         r: 100,
-  //         t: 100,
-  //         b: 100
-  //     }
-  // //render the plot to the div tag with id "bar"
-  // Plotly.newPlot("bar", chartBar, layout1);
+  axisY2:{
+    interlacedColor: "rgba(1,77,101,.2)",
+      gridColor: "rgba(1,77,101,.1)",
+      title: "Number of Passes"
+  },
+
+  data:[{
+    type: "bar",
+    name: "companies",
+    axisYType:"secondary",
+    color: "#014D65",
+    dataPoints:[
+      {y:data[0].Q219, label: 'Q219'},
+      {y:data[0].Q319, label:'Q319'},
+      {y:data[0].Q220, label:'Q220'},
+      {y:data[0].Q320, label: 'Q320'}
+    ]
+  }]
+});
+
+chart.render();
+
+}

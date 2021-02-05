2dd68420ae8602bfe007dece07f73d5bbfc84ade
spennington <spennington13@gmail.com>
2021-02-05 12:19:47 -0500

    update app.js
    still not able to load the total trips.  Not exactly sure what issue is. Looks like plotly error. index.html removed right column left column tags that help outline grid

--- static/js/app.js
+++ static/js/app.js
@@ -113,41 +113,44 @@ buildBar();
 console.log(data[8].Q319);
 
 function buildLine(){
-  var line1 = {
+  var trace1 = {
     x: ['Q2-19','Q3-19','Q2-20','Q3-20'],
     y: [data[8].Q219, data[8].Q319, data[8].Q220, data[8].Q320],
+    type: 'bar'
     // name: 'lines',
-    mode: 'lines',
-    line: {
-      width: 3,
-      color: 'red'
-    }
+    // mode: 'lines',
+    // line: {
+    //   width: 3,
+    //   color: 'red'
+    // }
   };
 
-  var line2 = {
+  var trace2 = {
     x: ['Q2-19','Q3-19','Q2-20','Q3-20'],
     y: [data[9].Q219,data[9].Q319,data[9].Q220,data[9].Q320],
+    type: 'bar'
     // name: 'lines',
-    mode: 'lines',
-    line: {
-      width: 3,
-      color: 'blue'
-    }
+    // mode: 'lines',
+    // line: {
+    //   width: 3,
+    //   color: 'blue'
+    // }
   };
 
-  var line3 = {
+  var trace3 = {
     x: ['Q2-19','Q3-19','Q2-20','Q3-20'],
     y: [data[10].Q219,data[10].Q319,data[10].Q220,data[10].Q320],
+    type: 'bar'
     // name: 'lines',
-    mode: 'line',
-    line: {
-      width: 3,
-      color: 'pink'
-    }
+    // mode: 'line',
+    // line: {
+    //   width: 3,
+    //   color: 'pink'
+    // }
   };
   
-  var info = {line1, line2, line3 };
+  var info2 = {trace1, trace2, trace3 };
 
-  Plotly.newPlot("total trips",info);
+  Plotly.newPlot("total trips", info2);
 }
 buildLine();
